9ede0c6c664723308a19972a1139c24b3ffd60a0
Shah rahman <toshah.rahman@gmail.com>
2022-05-19 17:40:21 +0300

    fast api fun step 03

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ app = FastAPI()
 
 
 @app.get("/")
-def index():
+def get_all_post():
     return {"message": "Hello World"}
+
+@app.post('/post')
+def create_post():
+    pass
+
+@app.put('/post/{id}')
+def update_a_post(id:int):
+    pass
+
+
+@app.delete('/post/{id}')
+def update_a_post(id:int):
+    pass
+
+
 
